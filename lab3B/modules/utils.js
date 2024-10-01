b7e00ab7e2211f2b57259cb17e84cb59a372dac0
Ruby <rchen139@my.bcit.ca>
2024-10-01 10:07:56 -0700

    Clean up the code

--- lab3B/modules/utils.js
+++ lab3B/modules/utils.js
@@ -1,9 +1,7 @@
 const fs = require('fs');
 const moment = require('moment-timezone');
 
-// Function to return the current time in Vancouver time
 function getDate() {
-    // Get current time in Vancouver's timezone
     return moment().tz("America/Vancouver").format('YYYY-MM-DD HH:mm:ss');
 }
 
@@ -14,7 +12,6 @@ function getGreeting(guest, timestamp){
         const greeting = JSON.parse(data);
         return greeting.greeting(guest, timestamp);
     }catch(err){
-        // Fallback message in case of an error
         return `Hello ${guest}, What a beautiful day. Server current date and time is ${timestamp}`;
     }
 }
